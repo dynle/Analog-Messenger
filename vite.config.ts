004491c9e5f74cbcd2ce451e8034136934c8dc6d
Doyoon Lee <dynleedev@gmail.com>
2022-06-04 22:50:28 +0900

    Update settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   // This changes the out put dir from dist to build
   // comment this out if that isn't relevant for your project
   server: {
-    open: '/index.html'
+    open: './index.html'
   },
   build: {
     outDir: 'build',
